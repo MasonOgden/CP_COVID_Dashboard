7988c06b2befd19bbb7688e3fc9e0f676202e5fa
Mason Ogden <msogden@calpoly.edu>
2020-10-20 16:34:03 -0700

    added landing page, fixed most visuals

--- CalPolyCovidDashboard/server.R
+++ CalPolyCovidDashboard/server.R
@@ -8,15 +8,224 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(plotly)
+library(lubridate)
+
+round_up_nearest <- function(num) {
+    num_zeros <- floor(log10(num))
+    divisor <- as.integer(str_c(c(1, rep(0, num_zeros)), collapse=""))
+    ceiling(num / divisor) * divisor
+}
+
+cp_ready <- read_csv("cp_dashboard_data.csv") %>%
+    mutate(updated = mdy(updated)) %>%
+    filter(wday(updated) %in% 2:6, # only keep weekdays (it's only updated on weekdays)
+           updated != "2020-10-13") %>% # we don't have data on this day 
+    mutate(daily_pos_on_campus = c(0, diff(total_pos_on_campus_res)),
+           .after = 'total_pos_on_campus_res') %>%
+    mutate(daily_pos_off_campus = c(0, diff(total_pos_off_campus_res)),
+           .after = 'total_pos_off_campus_res')
+
+font_settings <- list(
+    family = "Source Sans Pro"
+)
+
+# https://universitymarketing.calpoly.edu/brand-guidelines/typography/
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    set.seed(122)
     
-    hist_data <- rnorm(500)
+    activate <- 3
     
-    output$plot1 <- renderPlot({
-        data <- hist_data[seq_len(input$slider)]
-        hist(data)
+    observeEvent(once = TRUE, ignoreInit = FALSE, eventExpr = 'shiny:connected', {
+        # event will be called when activate changes, which is only once, when it is initially assigned
+        showModal(modalDialog(
+            box(title = p(h2('Welcome to the Cal Poly COVID-19 Visual Dashboard', style = 'font-family: Utopia; text-align: center'),
+                             br(),
+                             h4('Updated October 16th, 4:30 PM', style = 'font-family: Utopia; text-align: center')),
+                width = '100%'),
+            tags$img(src='cp_official2_wide2.jpg', style="display: block; margin-left: auto; margin-right: auto",
+                     width = '100%',
+                     height = '100%'),
+            br(),
+            fluidRow(
+                valueBox(20, p("New Positive Tests", br(), br(), style = 'line-height: 0.01cm'), icon = icon("chart-line"), color = 'red', width = 2),
+                valueBox(10000, "Total Tests Performed", icon = icon("plus-square"), color = 'green', width = 2),
+                valueBox(10, p("Students Currently",
+                          br(),
+                          p("in Isolation"),
+                          style = 'line-height: 0.01cm'), icon = icon('hourglass-end'), color = 'yellow', width = 2),
+                valueBox(10, p("Students Currently",
+                          br(),
+                          p("in Quarantine"),
+                          style = 'line-height: 0.01cm'), icon = icon('exclamation-triangle'), color = 'orange', width = 2),
+                valueBox(234, p("Residents Quarantined" ,
+                          br(),
+                          p("in Place"),
+                          style = 'line-height: 0.01cm'), icon = icon('house-user'), color = 'purple', width = 2),
+                valueBox(0, p("Residents Currently",
+                          br(),
+                          p("Hospitalized"),
+                          style = 'line-height: 0.01cm'), icon = icon('ambulance'), color = 'maroon', width = 2)
+            )#,
+            #h4('Created by Mason Ogden and Sydney Ozawa', style = 'text-align: center')
+        ))
+    })
+    
+    dates_to_show <- reactive({
+        input$date_slider
+    })
+    
+    plot_data <- reactive({cp_ready %>%
+        filter(updated >= first(dates_to_show()),
+               updated <= last(dates_to_show()))
+    })
+    
+    pie_data <- cp_ready %>%
+        select(total_pos_on_campus_res, total_pos_off_campus_res) %>%
+        slice_tail() %>%
+        pivot_longer(cols = c(total_pos_on_campus_res, total_pos_off_campus_res)) %>%
+        rename(location = name, num_cases = value) %>%
+        add_column(location_formatted = c("On-campus", "Off-campus"),
+                   .after = 'location')
+    
+    output$case_pie <- renderPlotly({
+        case_pie <- pie_data %>%
+        plot_ly(labels = ~location_formatted, values = ~num_cases,
+                marker = list(colors = c('#1b543b', '#9c9c80'))) %>%
+        add_pie(hole = 0.4,
+                text = str_c(pie_data$location_formatted, ': ', pie_data$num_cases),
+                hoverinfo = 'text') %>%
+        layout(xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               showlegend = FALSE,
+               font = font_settings) %>%
+        config(displaylogo = FALSE)
+    })
+        
+    
+    output$cp_daily_plot <- renderPlotly({
+        cp_daily_plot <- plot_data() %>%
+            plot_ly(type = 'bar') %>%
+            add_trace(x = ~updated,
+                      y = ~daily_pos_on_campus,
+                      name = 'On-Campus',
+                      text = str_c(strftime(plot_data()$updated, format="%b %d, %Y"), ':\n', plot_data()$daily_pos_on_campus, " new positive on-campus tests"),
+                      hoverinfo = 'text',
+                      marker = list(color = '#154734')) %>%
+            add_trace(x = ~updated,
+                      y = ~daily_pos_off_campus,
+                      name = 'Off-Campus',
+                      text = str_c(strftime(plot_data()$updated, format="%b %d, %Y"), ':\n', plot_data()$daily_pos_off_campus, " new positive off-campus tests"),
+                      hoverinfo = 'text',
+                      marker = list(color = '#F8E08E')) %>%
+            layout(yaxis = list(title = 'New Positive Tests'),
+                   xaxis = list(title = ''),
+                   barmode = 'stack',
+                   font = font_settings) %>%
+            config(displaylogo = FALSE)
+    })
+    
+    dates_to_show2 <- reactive({
+        input$date_slider2
+    })
+    
+    plot_data2 <- reactive({cp_ready %>%
+            filter(updated >= first(dates_to_show2()),
+                   updated <= last(dates_to_show2()))
+    })
+    
+    y_max_rounded <- reactive({
+        round_up_nearest(max(max(plot_data2()$total_tests_CHW), max(plot_data2()$total_tests_otp), na.rm = TRUE))
+    })
+    
+    output$testing_totals_plot <- renderPlotly({
+        testing_totals_plot <- plot_data2() %>%
+            plot_ly(type = 'scatter', mode = 'lines+markers') %>%
+            add_trace(x = ~updated, 
+                      y = ~total_tests_CHW,
+                      color = I("#154734"),
+                      name = "Campus Health and Wellbeing (since Jul. 8, 2020)",
+                      text = str_c(strftime(plot_data2()$updated, format="%b %d, %Y"), ':\n', plot_data2()$total_tests_CHW, ' total CHW tests performed'),
+                      hoverinfo = 'text') %>%
+            add_trace(x = ~updated,
+                      y = ~total_tests_otp,
+                      color = I("#F8E08E"),
+                      name = "Ongoing Testing Program (since Oct. 3, 2020)",
+                      text = str_c(strftime(plot_data2()$updated, format="%b %d, %Y"), ':\n', plot_data2()$total_tests_otp, ' total OTP tests performed'),
+                      hoverinfo = 'text') %>%
+            layout(yaxis = list(range = c(0, y_max_rounded()),
+                                title = "Total Tests Performed"),
+                   xaxis = list(title = ""),
+                   legend = list(x = 0.03, y = 0.94),
+                   font = font_settings) %>%
+            config(displaylogo = FALSE)
+    })
+    
+       
+    dates_to_show3 <- reactive({
+        input$date_slider3
+    })    
+     
+    plot_data3 <- reactive({cp_ready %>%
+        filter(updated >= first(dates_to_show3()),
+               updated <= last(dates_to_show3()))
+    })
+    
+    y_max_rounded2 <- reactive({
+        round_up_nearest(max(max(plot_data3()$total_on_campus_res_iso), max(plot_data3()$total_on_campus_res_quar), na.rm = TRUE))
+    })
+        
+    output$iso_quar_totals_plot <- renderPlotly({plot_data3() %>%
+        plot_ly(type = 'scatter', mode = 'markers+lines') %>%
+        add_trace(x = ~updated,
+                  y = ~total_on_campus_res_iso,
+                  color = I('#154734'),
+                  name = 'Isolation', 
+                  text = str_c(strftime(plot_data3()$updated, format="%b %d, %Y"), ':\n', plot_data3()$total_on_campus_res_iso, ' total resident students in isolation'),
+                  hoverinfo = 'text') %>%
+        add_trace(x = ~updated,
+                  y = ~total_on_campus_res_quar,
+                  color = I('#F8E08E'),
+                  name = 'Quarantine', 
+                  text = str_c(strftime(plot_data3()$updated, format="%b %d, %Y"), ':\n', plot_data3()$total_on_campus_res_quar, ' total resident students in isolation'),
+                  hoverinfo = 'text') %>%
+        layout(yaxis = list(title = "Number of Resident Students",
+                            range = c(0, y_max_rounded2())),
+               xaxis = list(title = ''),
+               legend = list(x = 0.03, y = 0.94),
+               font = font_settings) %>%
+        config(displaylogo = FALSE)
+    })
+    
+    dates_to_show4 <- reactive({
+        input$date_slider4
+    })    
+    
+    plot_data4 <- reactive({cp_ready %>%
+            filter(updated >= first(dates_to_show4()),
+                   updated <= last(dates_to_show4()))
+    })
+    
+    y_max_rounded3 <- reactive({
+        round_up_nearest(max(plot_data4()$total_on_quar_current_quar_in_place, na.rm = TRUE))
+    })
+    
+    output$quar_in_place_plot <- renderPlotly({
+        plot_data4() %>%
+            plot_ly(type = 'scatter', mode = 'markers+lines') %>%
+            add_trace(x = ~updated,
+                      y = ~total_on_quar_current_quar_in_place,
+                      color = I('#154734'),
+                      text = str_c(strftime(plot_data4()$updated, format="%b %d, %Y"), ':\n', plot_data4()$total_on_quar_current_quar_in_place, ' total resident students'),
+                      hoverinfo = 'text') %>%
+            layout(showlegend=FALSE,
+                   xaxis = list(title = ''),
+                   yaxis = list(title = 'Number of Resident Students',
+                   range = c(0, y_max_rounded3())),
+                   font = font_settings) %>%
+            config(displaylogo = FALSE)
+            
     })
 })
--- CalPolyCovidDashboard/ui.R
+++ CalPolyCovidDashboard/ui.R
@@ -6,22 +6,236 @@
 #
 #    http://shiny.rstudio.com/
 #
+# first-aid
 
 library(shiny)
+library(plotly)
+library(tidyverse)
+library(lubridate)
 library(shinydashboard)
+library(dashboardthemes)
+
+### creating custom theme object
+customTheme <- shinyDashboardThemeDIY(
+    
+    ### general
+    appFontFamily = "Source Sans Pro" # Font of everything
+    ,appFontColor = "rgb(45,45,45)"
+    ,primaryFontColor = "rgb(15,15,15)"
+    ,infoFontColor = "rgb(15,15,15)"
+    ,successFontColor = "rgb(15,15,15)"
+    ,warningFontColor = "rgb(15,15,15)"
+    ,dangerFontColor = "rgb(15,15,15)"
+    ,bodyBackColor = "rgb(240,240,240)" # background of body (behind the boxes)
+    
+    ### header
+    ,logoBackColor = "#154734" # background of header (top)
+    
+    ,headerButtonBackColor = "#154734" # background of menu button background when mouse is not over it
+    ,headerButtonIconColor = "rgb(220,220,220)" # color of menu icon when mouse is not over it
+    ,headerButtonBackColorHover = "#3A913F" # color of menu button background when mouse is over it
+    ,headerButtonIconColorHover = "rgb(60,60,60)" # color menu button icon when mouse is over it
+    
+    ,headerBackColor = "#154734" # Top bar to the right of header (not including button)
+    ,headerBoxShadowColor = "#dfdfdf" # not sure what this does
+    ,headerBoxShadowSize = "3px 5px 5px" # or this
+    
+    ### sidebar
+    ,sidebarBackColor = "rgb(255,255,255)" # background of entire sidebar
+    ,sidebarPadding = 0 # how much space on each side of tabs in sidebar
+    
+    ,sidebarMenuBackColor = "transparent" # background color of non-active tabs in sidebar
+    ,sidebarMenuPadding = 0 # how much space on each side of tabs, and on top and bottom in sidebar
+    ,sidebarMenuBorderRadius = 0 # makes the current sidebar overlay color curved at the edges (like in blue gradient)
+    
+    ,sidebarShadowRadius = "3px 5px 5px" # size of sidebar shadow
+    ,sidebarShadowColor = "#dfdfdf" # color of shadow on right side of sidebar (between sidebar and body)
+    
+    ,sidebarUserTextColor = "rgb(115,115,115)" # not sure
+    
+    ,sidebarSearchBackColor = "rgb(240,240,240)"   # \
+    ,sidebarSearchIconColor = "rgb(100,100,100)"   # |- control colors of search bar, which I don't have
+    ,sidebarSearchBorderColor = "rgb(220,220,220)" # /
+    
+    ,sidebarTabTextColor = "rgb(100,100,100)" # color of text in sidebar tabs
+    ,sidebarTabTextSize = 14 # size of text in sidebar tabs
+    ,sidebarTabBorderStyle = "none" # not sure
+    ,sidebarTabBorderColor = "none" # not sure
+    ,sidebarTabBorderWidth = 0 # not sure
+    
+    ,sidebarTabBackColorSelected = "rgb(230,230,230)" # color of active tab rgb(230,230,230) #3A913F
+    ,sidebarTabTextColorSelected = "rgb(0,0,0)" # color of text of active tab (gets darker when selected)
+    ,sidebarTabRadiusSelected = "0px" # makes selection highlight rounded at the edges
+    
+    ,sidebarTabBackColorHover = "rgb(245,245,245)" # background color of tab you're hovering over rgb(245,245,245) #A4D65E
+    ,sidebarTabTextColorHover = "rgb(0,0,0)" # text color of tab you're hovering over (gets darker)
+    ,sidebarTabBorderStyleHover = "none solid none none" # 'solid' adds the vertical solid line on right side of active tab
+    ,sidebarTabBorderColorHover = "rgb(200,200,200)" # color of vertical solid line on right side of active tab
+    ,sidebarTabBorderWidthHover = 4 # width of vertical solid line
+    ,sidebarTabRadiusHover = "0px" # rounds edges of hovering highlight
+    
+    ,boxBackColor = "rgb(248,248,248)" # background color of boxes
+    ,boxBorderRadius = 0 # how much to round the edges of the boxes
+    ,boxShadowSize = "none" # toggle box shadows
+    ,boxShadowColor = "" # color of box shadow
+    ,boxTitleSize = 18 # font size of box title
+    ,boxDefaultColor = "rgb(248,248,248)" # changes color of little horizontal bar on top of some boxes (like my text boxes) rgb(225,225,225)
+    ,boxPrimaryColor = "rgb(95,155,213)" # \
+    ,boxInfoColor = "rgb(180,180,180)"   #  \
+    ,boxSuccessColor = "rgb(112,173,71)" #   |- control horizontal bar colors of different statuses
+    ,boxWarningColor = "rgb(237,125,49)" #  /
+    ,boxDangerColor = "rgb(232,76,34)"   # /
+    
+    ,tabBoxTabColor = "rgb(248,248,248)"          # \
+    ,tabBoxTabTextSize = 14                       #  \
+    ,tabBoxTabTextColor = "rgb(100,100,100)"      #   \ 
+    ,tabBoxTabTextColorSelected = "rgb(45,45,45)" #    |- settings for if you have boxes with tabs
+    ,tabBoxBackColor = "rgb(248,248,248)"         #   /
+    ,tabBoxHighlightColor = "rgb(200,200,200)"    #  /
+    ,tabBoxBorderRadius = 5                       # /
+    
+    ### inputs
+    ,buttonBackColor = "rgb(215,215,215)" # not sure
+    ,buttonTextColor = "rgb(45,45,45)" # not sure
+    ,buttonBorderColor = "rgb(150,150,150)" # not sure
+    ,buttonBorderRadius = 5 # not sure
+    
+    ,buttonBackColorHover = "rgb(190,190,190)" # not sure
+    ,buttonTextColorHover = "rgb(0,0,0)" # not sure
+    ,buttonBorderColorHover = "rgb(150,150,150)" # not sure
+    
+    ,textboxBackColor = "rgb(255,255,255)" # not sure
+    ,textboxBorderColor = "rgb(118,118,118)" # not sure
+    ,textboxBorderRadius = 5 # not sure
+    ,textboxBackColorSelect = "rgb(245,245,245)" # not sure
+    ,textboxBorderColorSelect = "rgb(108,108,108)" # not sure
+    
+    ### tables                             # \
+    ,tableBackColor = "rgb(248,248,248)"   #  \
+    ,tableBorderColor = "rgb(238,238,238)" #   |- settings for if you display tables
+    ,tableBorderTopSize = 1                #  /
+    ,tableBorderRowSize = 1                # /
+    
+)
+
+cp_ready <- read_csv("cp_dashboard_data.csv") %>%
+    mutate(updated = mdy(updated)) %>%
+    filter(wday(updated) %in% 2:6, # only keep weekdays (it's only updated on weekdays)
+           updated != "2020-10-13")
+
+dates <- cp_ready %>%
+    pull(updated)
+
+qip_begin_index <- min(which(!is.na(cp_ready$total_on_quar_current_quar_in_place)))
 
 
+
+# Cal Poly COVID-19 Visual Dashboard
 dashboardPage(
-    dashboardHeader(title = "Cal Poly COVID-19 Visual Dashboard"),
-    dashboardSidebar(),
+    dashboardHeader(title = span("Cal Poly COVID-19 Visual Dashboard", style = "color: white; font-family: Utopia; font-size: 22px"),
+                    titleWidth = 375),
+    dashboardSidebar(
+        collapsed = FALSE,
+        width = 375, # 417 is width + button but shadow overlaps
+        sidebarMenu(
+            menuItem(span("Daily New Cases", style = "font-family: Utopia; font-size: 18px"), tabName = "daily_stats", icon = icon("chart-line")),
+            menuItem(span("Testing", style = "font-family: Utopia; font-size: 18px"), tabName = "testing", icon = icon("plus-square")),
+            menuItem(span("Isolation & Quarantine", style = "font-family: Utopia; font-size: 18px"), tabName = "iso_quar", icon = icon("exclamation-triangle")),
+            menuItem(span("Quarantine in Place", style = "font-family: Utopia; font-size: 18px"), tabName = "quar_in_place", icon = icon('door-closed'))
+        )
+    ),
     dashboardBody(
-        fluidRow(
-            box(plotOutput("plot1", height=250)),
+        tags$head(tags$style(HTML('
+            .modal.in .modal-dialog{
+                width:100%;
+                height:100%;
+                margin:0px;
+            }
             
-            box(
-                title="Controls",
-                sliderInput("slider", "Number of observations", 1, 100, 50)
-            )
+            .modal-content{
+                width:100%;
+                height:100%;
+            }
+         '))),
+        
+        customTheme,
+        
+        tabItems(
+            tabItem(tabName = "daily_stats",
+                fluidRow(
+                    box(title = span("Cal Poly Daily Positive COVID-19 Cases", style = "font-family: Utopia; font-size: 20px"),
+                        solidHeader = TRUE,
+                        plotlyOutput("cp_daily_plot"),
+                        width = 8),
+            
+                    box(width = 4,
+                        title = span("Controls", style = "font-family: Utopia; font-size: 20px"),
+                        solidHeader = TRUE,
+                        dateRangeInput("date_slider", label = "Dates to show:", min = first(dates),
+                                       start = first(dates), end = last(dates), max = last(dates),
+                                       format = 'mm-dd-yyyy'), 
+                        footer = p(strong('Note:'), br(), "Data updated Monday through Friday. Numbers reflect data from previous weekday.")
+                    )
+                )
+            ),
+            tabItem(tabName = 'testing',
+                    fluidRow(
+                        box(title = span("Cal Poly Testing", style = "font-family: Utopia; font-size: 20px"),
+                            plotlyOutput("testing_totals_plot"),
+                            width = 8),
+                        
+                        box(width = 4,
+                            title = span("Controls", style = "font-family: Utopia; font-size: 20px"),
+                            dateRangeInput("date_slider2", label = "Dates to show:", min = first(dates),
+                                           start = first(dates), end = last(dates), max = last(dates),
+                                           format = 'mm-dd-yyyy'),
+                            footer = p(strong('Note:'), br(), 'Cal Poly provides COVID-19 testing through its Campus Health and Wellbeing only to currently enrolled students. 
+                                        These numbers reflect only positive cases identified directly through Campus Health and Wellbeing. 
+                                        Students can also access testing off campus.')
+                        )
+                    )
+            ),
+            tabItem(tabName = 'iso_quar',
+                    fluidRow(
+                        box(title = span("Total On-Campus Resident Students in Isolation/Quarantine", style = "font-family: Utopia; font-size: 20px"),
+                            plotlyOutput('iso_quar_totals_plot'),
+                            width = 8),
+                        
+                        box(width = 4,
+                            title = span("Controls", style = "font-family: Utopia; font-size: 20px"),
+                            dateRangeInput("date_slider3", label = "Dates to show:", min = first(dates),
+                                           start = first(dates), end = last(dates), max = last(dates),
+                                           format = 'mm-dd-yyyy'),
+                            footer = p(strong('Isolation:'), br(), 'Students who test positive for COVID-19 are placed in isolation in on-campus University Housing facilities and 
+                                       provided with direct dining, laundry and other support services. Per the CDC, an individual who has tested positive
+                                       can leave isolation if it has been 10 days since their symptoms first appeared, they haven’t had a fever in at least 
+                                       24 hours without the use of fever-reducing medications, and their symptoms have improved.',
+                                       br(),
+                                       br(),
+                                       strong('Quarantine:'),
+                                       br(),
+                                       'Students who have been exposed to someone with COVID-19 must self-quarantine for 14 days by county health order.')
+                        )
+                    )
+                
+            ),
+            tabItem(tabName = 'quar_in_place',
+                    fluidRow(
+                        box(title = span("Total On-Campus Resident Students Currenly Quarantining in Place", style = "font-family: Utopia; font-size: 20px"),
+                            plotlyOutput("quar_in_place_plot"),
+                            width = 8),
+                        
+                        box(width = 4,
+                            title = span("Controls", style = "font-family: Utopia; font-size: 20px"),
+                            dateRangeInput("date_slider4", label = 'Dates to show', min = dates[qip_begin_index],
+                                           start = dates[qip_begin_index], end = last(dates), max = last(dates),
+                                           format = 'mm-dd-yyyy'),
+                            footer = p(strong('Quarantine in Place:'), br(), 'In the event of possible exposures in University Housing facilities, the university can, as a precaution, place 
+                                        floors or sections of buildings on “Quarantine in Place” for 14 days, in consultation with San Luis Obispo County
+                                        Department of Public Health.')
+                        )
+                    )
+                )
         )
     )
 )
